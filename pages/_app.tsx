8cb96aaf32a72974656730618e88fbe476aada1d
Nickothan <s1nicolasrg@gmail.com>
2022-06-06 08:56:02 -0500

    implementando global styles y theme basico.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,43 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    color: ${theme.colors.primary} ;
   }
-`
+
+  h1 {font-size: 64px;}
+  h2 {font-size: 56px;}
+  h3 {font-size: 48px;}
+  h4 {font-size: 28px;}
+  h5 {font-size: 20px;}
+  h6 {font-size: 12px;}
+  p, a {font-size: 12px;}
+`;
 
 const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+	colors: {
+		primary: "#333333",
+		gary: "#DDDDDD",
+		green: "#6BDFDC",
+		blue: "#0077B6",
+		success: "#42C576",
+		danger: "#FF637D",
+		warning: "#FFF87F",
+		white1: "#F1F1F1",
+		white2: "#F3F3F3",
+	},
+};
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+	return (
+		<>
+			<GlobalStyle />
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
---
+++ pages/log in/index.js
@@ -0,0 +1,9 @@
+const LogIn = () => {
+	return (
+		<>
+			<form>
+				<h2>Log In</h2>
+			</form>
+		</>
+	);
+};
