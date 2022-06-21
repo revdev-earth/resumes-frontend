f70dbc3e50e95ea9b42480ce93d9911be2202b65
Nickothan <s1nicolasrg@gmail.com>
2022-06-21 08:36:14 -0500

    Update files.

--- pages/templates/Template1/_components/Social/index.tsx
+++ pages/templates/Template1/_components/Social/index.tsx
@@ -1,15 +1,16 @@
 import { Title } from "../../styles"
+import Image from "next/image"
 
 const Social = ({ social }) => {
   return (
     <div>
       <Title>Social</Title>
-      {social.map(({ title, link }) => {
+      {social.map(({ link }) => {
         ;<a href={link} key={Math.random() * 10}>
-          <img src="/assets/link.svg" alt="" />
+          <Image src="/assets/link.svg" alt="" />
         </a>
       })}
-      <img src="" alt="" />
+      <Image src="" alt="" />
     </div>
   )
 }
