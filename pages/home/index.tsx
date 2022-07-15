20f57db0bb0559e6ac9722b6549b3d38631b3854
Revi <oliiv@outlook.com>
2022-07-15 12:53:55 +0200

    start markup

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -6,8 +6,24 @@ const Home = () => {
       <Header />
 
       <main>
+        {/* Section 1 */}
         <section className="w-full h-16 bg-slate-400 rounded-full">
-          <div>etwas</div>
+          <div>In a jiffy, show who you are</div>
+          <div>
+            You can create your CV in just a few minutes and share it with
+            potential employers
+          </div>
+          <button>Get started</button>
+          <div>
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
         </section>
       </main>
       <footer></footer>
