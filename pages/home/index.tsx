4718edd27797e554cd190c96a464350e34a8223a
Revi <oliiv@outlook.com>
2022-07-15 14:07:50 +0200

    first section styles progress

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -7,13 +7,19 @@ const Home = () => {
 
       <main>
         {/* Section 1 */}
-        <section className="w-full h-16 bg-slate-400 rounded-full">
-          <div>In a jiffy, show who you are</div>
-          <div>
-            You can create your CV in just a few minutes and share it with
-            potential employers
+        <section className="flex flex-col items-center text-center w-full p-8 bg-[#0077B6] text-white">
+          <div className="flex flex-col items-center gap-9 max-w-[600px]">
+            <div className="text-5xl font-bold">
+              In a jiffy, show who you are
+            </div>
+            <div className="text-3xl max-w-[400px]">
+              You can create your CV in just a few minutes and share it with
+              potential employers
+            </div>
+            <button className="w-[500px] h-[100px] rounded-lg bg-[#90E0EF] shadow-xl hover:shadow-md transition text-4xl font-bold">
+              Get started
+            </button>
           </div>
-          <button>Get started</button>
           <div>
             <div>
               <div></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ["./pages/**/*.tsx", "./src/**/*.tsx"],
   theme: {
     extend: {
       keyframes: {
