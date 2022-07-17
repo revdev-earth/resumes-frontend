8a216253974256a39782f2797e44d2350abd982e
Revi <oliiv@outlook.com>
2022-07-17 12:14:23 +0200

    footer

--- src/components/principal/Footer/index.tsx
+++ src/components/principal/Footer/index.tsx
@@ -1,9 +1,58 @@
+const footer_items = [
+  {
+    id: "1",
+    title: "Resumes",
+    items: ["How we are?", "what we make?", "Our company"],
+  },
+  {
+    id: "2",
+    title: "Community",
+    items: ["Carrier", "Participate", "Information", "Blog"],
+  },
+  {
+    id: "3",
+    title: "Contact Us",
+    items: ["email"],
+  },
+]
+
+export const Space = ({ title, items }) => (
+  <>
+    {/* 
+          message */}
+    <div className=" flex flex-col gap-[14px] h-min ">
+      {/* 
+            name */}
+      <div className="text-[35px] leading-[52.5px] text-[#0077B6] font-bold">
+        {title}
+      </div>
+
+      <div>
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="text-[28px] leading-[44.8px] text-[#0077B6]"
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  </>
+)
+
 export const Footer = () => (
-  <section className="flex flex-col items-center text-center w-full p-28 bg-[#CAF0F8] text-white">
-    <div className="flex gap-24">
-      <div className="bg-[#90E0EF] h-40 w-40  rounded transition shadow-md hover:shadow-xl" />
-      <div className="bg-[#90E0EF] h-40 w-40  rounded transition shadow-md hover:shadow-xl" />
-      <div className="bg-[#90E0EF] h-40 w-40  rounded transition shadow-md hover:shadow-xl" />
+  <section
+    className="
+    text-white pt-[75px]
+    h-[400px] w-full
+    bg-[#CAF0F8]
+     "
+  >
+    <div className="flex justify-between w-[850px] m-auto">
+      {footer_items.map((item) => (
+        <Space key={item.id} {...item} />
+      ))}
     </div>
   </section>
 )
