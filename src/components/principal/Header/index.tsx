0646975723e5f92f5f23d38677f2c5954fc30a2f
Revi <oliiv@outlook.com>
2022-07-17 12:32:30 +0200

    404 and home

--- src/components/principal/Header/index.tsx
+++ src/components/principal/Header/index.tsx
@@ -18,36 +18,49 @@ export const Header = () => {
   }
 
   return (
-    <header className="flex justify-between items-center px-24 bg-[#CAF0F8] drop-shadow-lg h-24">
-      <Link href="/">
-        {/* Logo */}
-        <div
-          className="text-5xl font-bold cursor-pointer"
-          style={{
-            textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-            color: "#0077B6",
-          }}
-        >
-          Resumes
+    <header className="flex bg-[#CAF0F8] drop-shadow-lg h-24">
+      <div className="flex justify-between items-center w-[900px] m-auto">
+        <Link href="home">
+          {/* Logo */}
+          <div
+            className="text-5xl font-bold cursor-pointer"
+            style={{
+              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+              color: "#0077B6",
+            }}
+          >
+            Resumes
+          </div>
+        </Link>
+
+        <div className="relative">
+          {/* Perfil */}
+          <div
+            className="w-16 h-16 bg-[#D9D9D9] rounded-full cursor-pointer"
+            style={{
+              boxShadow:
+                "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 2px 4px rgba(0, 0, 0, 0.25), inset 0px -2px 4px rgba(0, 0, 0, 0.25)",
+            }}
+            {...commonEvents}
+          />
+
+          {/* Menu */}
+          {showMenu && (
+            <nav className="absolute left-[-12%]" {...commonEvents}>
+              <div
+                className="
+                  flex flex-col gap-4 p-3 
+                  bg-white drop-shadow-xl 
+                  rounded
+                  text-[29px]
+                  "
+              >
+                <Link href="/login">Log In</Link>
+                <Link href="/create_account">Register</Link>
+              </div>
+            </nav>
+          )}
         </div>
-      </Link>
-
-      <div className="relative">
-        {/* Perfil */}
-        <div
-          className="w-16 h-16 bg-slate-400 rounded-full cursor-pointer"
-          {...commonEvents}
-        />
-
-        {/* Menu */}
-        {showMenu && (
-          <nav className="absolute left-[-12%]" {...commonEvents}>
-            <div className="flex flex-col gap-4 p-3 bg-white drop-shadow-xl rounded animate-fade-in">
-              <Link href="/login">Log In</Link>
-              <Link href="/create_account">Register</Link>
-            </div>
-          </nav>
-        )}
       </div>
     </header>
   )
