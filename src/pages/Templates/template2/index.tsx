4653fdc424c1d30e7f980ded15f60611ee9689e5
Revi <oliiv@outlook.com>
2022-07-18 15:14:21 +0200

    Who I am

--- src/pages/Templates/template2/index.tsx
+++ src/pages/Templates/template2/index.tsx
@@ -22,6 +22,21 @@ export const Template2 = () => {
           <main>
             <section>
               {/* Who I am */}
+              <div>
+                {/* Title */}
+                <div>Who I am:</div>
+
+                {/* Content */}
+                <div>
+                  Solution-seeking is my forte. With my skills and experience, I
+                  am able to achieve a wide range of complex and scalable
+                  projects.
+                  <br />
+                  <br />
+                  Through the application of new knowledge, I am always seeking
+                  to improve my services.
+                </div>
+              </div>
 
               {/* Experiences */}
 
