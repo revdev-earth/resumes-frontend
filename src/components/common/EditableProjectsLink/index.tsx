abffd4d17b9642ce5adf628e4b114f13f364aea4
Revi <oliiv@outlook.com>
2022-09-02 14:45:28 +0200

    Editables

--- src/components/common/EditableProjectsLink/index.tsx
+++ src/components/common/EditableProjectsLink/index.tsx
@@ -1,7 +1,9 @@
-//field
+import Image from "next/image"
+
+import { useSelector } from "@redux"
 
 import { checkPublicIcon } from "@utils"
-import Image from "next/image"
+
 import { useEdition } from "./_components"
 
 interface EditableProps {
@@ -19,6 +21,10 @@ export const EditableProjectsLink = ({
   stylesText,
   link: link_icoming,
 }: EditableProps) => {
+  //
+
+  const { role } = useSelector((s) => s.app.auth)
+
   const {
     availableToEdit,
     handleClickActiveEdition,
@@ -29,78 +35,71 @@ export const EditableProjectsLink = ({
     refElement,
   } = useEdition({ link: link_icoming, name })
 
+  // Text area
+
+  const text_area_style = `
+    bg-transparent h-[30px] pl-2 border-[1px]
+    border-solid rounded-md border-[#6BDFDC]
+    ${stylesText}
+  `
+
+  const text_area_props = (name: "icon" | "path" | "name") => ({
+    onChange: handleWriting,
+    onDoubleClick: handleClickSave,
+    name,
+    value: link[name],
+    className: text_area_style,
+    style: {
+      width: sizes.offsetWidth,
+    },
+  })
+
+  // Div
+
+  const div_style = `
+    border-solid rounded-md border-1 
+    border-transparent p-[1px] mb-[6px]
+    ${stylesText}
+  `
+
+  const content_reader = (
+    <div
+      className={`font-bold flex gap-1 ${
+        role === "reader" && "cursor-pointer"
+      }`}
+    >
+      <Image
+        src={checkPublicIcon(link.icon)}
+        alt="icon link project"
+        width={16}
+        height={16}
+        className={`${role === "reader" && "cursor-pointer"}`}
+      />
+      {link.name}
+    </div>
+  )
+
   return (
     <>
       {availableToEdit ? (
         <>
-          <textarea
-            className={`
-            bg-transparent h-[30px] pl-2
-            border-solid rounded-md border-[1px] border-[#6BDFDC]
-            ${stylesText}
-          `}
-            style={{
-              width: sizes.offsetWidth,
-              resize: "none",
-            }}
-            onChange={handleWriting}
-            onDoubleClick={handleClickSave}
-            name="icon"
-            value={link.icon}
-          />
-          <textarea
-            className={`
-            bg-transparent h-[30px] pl-2
-            border-solid rounded-md border-[1px] border-[#6BDFDC]
-            ${stylesText}
-          `}
-            style={{
-              width: sizes.offsetWidth,
-              resize: "none",
-            }}
-            onChange={handleWriting}
-            onDoubleClick={handleClickSave}
-            name="path"
-            value={link.path}
-          />
-
-          <textarea
-            className={`
-            bg-transparent h-[30px] pl-2
-            border-solid rounded-md border-[1px] border-[#6BDFDC]
-            ${stylesText}
-          `}
-            style={{
-              width: sizes.offsetWidth,
-              resize: "none",
-            }}
-            onChange={handleWriting}
-            onDoubleClick={handleClickSave}
-            name="name"
-            value={link.name}
-          />
+          <textarea {...text_area_props("icon")} />
+          <textarea {...text_area_props("path")} />
+          <textarea {...text_area_props("name")} />
         </>
       ) : (
         <div
-          className={`
-            border-solid rounded-md border-1 
-            border-transparent p-[1px] mb-[6px]
-        ${stylesText}
-      `}
+          className={div_style}
           ref={refElement}
           onDoubleClick={handleClickActiveEdition}
         >
-          {/* <a className="font-bold" href={path}> */}
-          <div className="font-bold flex gap-1">
-            <Image
-              src={checkPublicIcon(link.icon)}
-              alt="icon link project"
-              width={16}
-              height={16}
-            />
-            {link.name}
-          </div>
-          {/* </a> */}
+          {role === "writer" && content_reader}
+
+          {role === "reader" && (
+            <a className="font-bold" href={link.path}>
+              {content_reader}
+            </a>
+          )}
         </div>
       )}
     </>
--- src/components/common/EditableSocial/index.tsx
+++ src/components/common/EditableSocial/index.tsx
@@ -1,7 +1,8 @@
-//field
+import Image from "next/image"
+
+import { useSelector } from "@redux"
 
 import { checkPublicIcon } from "@utils"
-import Image from "next/image"
 import { Social_item } from "tree"
 import { useEdition } from "./_components"
 
@@ -16,6 +17,10 @@ export const EditableSocial = ({
   stylesText,
   social: social_icoming,
 }: EditableProps) => {
+  //
+
+  const { role } = useSelector((s) => s.app.auth)
+
   const {
     availableToEdit,
     handleClickActiveEdition,
@@ -26,78 +31,71 @@ export const EditableSocial = ({
     refElement,
   } = useEdition({ social: social_icoming, name })
 
+  // Text area
+
+  const text_area_style = `
+    bg-transparent h-[30px] pl-2 border-[1px]
+    border-solid rounded-md border-[#6BDFDC]
+    ${stylesText}
+  `
+
+  const text_area_props = (name: "icon" | "path" | "name") => ({
+    onChange: handleWriting,
+    onDoubleClick: handleClickSave,
+    name,
+    value: social[name],
+    className: text_area_style,
+    style: {
+      width: sizes.offsetWidth,
+    },
+  })
+
+  // Div
+
+  const div_style = `
+    border-solid rounded-md border-1 
+    border-transparent p-[1px] mb-[6px]
+    ${stylesText}
+  `
+
+  const content_reader = (
+    <div
+      className={`font-bold flex gap-1 ${
+        role === "reader" && "cursor-pointer"
+      }`}
+    >
+      <Image
+        src={checkPublicIcon(social.icon)}
+        alt="icon social media"
+        width={16}
+        height={16}
+        className={`${role === "reader" && "cursor-pointer"}`}
+      />
+      {social.name}
+    </div>
+  )
+
   return (
     <>
       {availableToEdit ? (
         <>
-          <textarea
-            className={`
-            bg-transparent h-[30px] pl-2
-            border-solid rounded-md border-[1px] border-[#6BDFDC]
-            ${stylesText}
-          `}
-            style={{
-              width: sizes.offsetWidth,
-              resize: "none",
-            }}
-            onChange={handleWriting}
-            onDoubleClick={handleClickSave}
-            name="icon"
-            value={social.icon}
-          />
-          <textarea
-            className={`
-            bg-transparent h-[30px] pl-2
-            border-solid rounded-md border-[1px] border-[#6BDFDC]
-            ${stylesText}
-          `}
-            style={{
-              width: sizes.offsetWidth,
-              resize: "none",
-            }}
-            onChange={handleWriting}
-            onDoubleClick={handleClickSave}
-            name="path"
-            value={social.path}
-          />
-
-          <textarea
-            className={`
-            bg-transparent h-[30px] pl-2
-            border-solid rounded-md border-[1px] border-[#6BDFDC]
-            ${stylesText}
-          `}
-            style={{
-              width: sizes.offsetWidth,
-              resize: "none",
-            }}
-            onChange={handleWriting}
-            onDoubleClick={handleClickSave}
-            name="name"
-            value={social.name}
-          />
+          <textarea {...text_area_props("icon")} />
+          <textarea {...text_area_props("path")} />
+          <textarea {...text_area_props("name")} />
         </>
       ) : (
         <div
-          className={`
-            border-solid rounded-md border-1 
-            border-transparent p-[1px] mb-[6px]
-        ${stylesText}
-      `}
+          className={div_style}
           ref={refElement}
           onDoubleClick={handleClickActiveEdition}
         >
-          {/* <a className="font-bold" href={path}> */}
-          <div className="font-bold flex gap-1">
-            <Image
-              src={checkPublicIcon(social.icon)}
-              alt=""
-              width={16}
-              height={16}
-            />
-            {social.name}
-          </div>
-          {/* </a> */}
+          {role === "writer" && content_reader}
+
+          {role === "reader" && (
+            <a className="font-bold" href={social.path}>
+              {content_reader}
+            </a>
+          )}
         </div>
       )}
     </>
